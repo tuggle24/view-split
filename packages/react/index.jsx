136c9f8943caa08469d328ce8bc0345de36dcbdb
tuggle.24 <tuggle.24@osu.edu>
2021-08-29 13:58:38 -0400

    feat: refactor react logic & added boilerplate vue

--- packages/react/index.jsx
+++ packages/react/index.jsx
@@ -1,146 +1,77 @@
-import React, { useState, useEffect } from "react";
-// import divider from "divider-html";
+import React, { useState, useEffect, Children, cloneElement } from "react";
 
-function closeOver (stuff, logic) {
-  return function handler (event) {
-    return logic(event, stuff)
-  }
-}
-
-function handleMouseUp (moveHandler) {
-  return function handler (event) {
+function handleMouseUp(moveHandler) {
+  return function handler(event) {
     event.preventDefault();
 
-    console.log('here')
     document.removeEventListener("mousemove", moveHandler);
     document.removeEventListener("mouseup", handler);
-    // document.removeEventListener("mouseup", this.stop);
-  }
+  };
 }
 
-function handleMouseMove (dragOffset, previousSiblingStart) {
-  return function handler (event) {
+function handleMouseMove(
+  dragOffset,
+  mouseDownEvent,
+  currentSizes,
+  paintScreen
+) {
+  return function handler(mouseMoveEvent) {
+    mouseMoveEvent.preventDefault();
+
+    const offset =
+      mouseMoveEvent.clientX -
+      mouseDownEvent.target.previousSibling.getBoundingClientRect().left +
+      ((10 * 1) / 2 - dragOffset);
+
+    const size =
+      ((10 * 1) / 2) * 1 +
+      mouseDownEvent.target.previousSibling.getBoundingClientRect().width +
+      mouseDownEvent.target.nextSibling.getBoundingClientRect().width;
+
+    const totalPer = currentSizes[0] + currentSizes[1];
+    const previousSiblingSize = (offset / size) * totalPer;
+    const nextSiblingSize = totalPer - (offset / size) * totalPer;
+
+    paintScreen([previousSiblingSize, nextSiblingSize]);
+  };
+}
+
+export default function Tilt({ children }) {
+  const [sizes, updateSizes] = useState([50, 50]);
+
+  function renderSize(sizes) {
+    updateSizes((prevSizes) => sizes);
+  }
+
+  function handleDown(event) {
     event.preventDefault();
-    console.log('added')
+    const dragOffset =
+      event.clientX - event.target.getBoundingClientRect().left;
 
-    // const offset = event.clientX - divider.element.previousSibling + ((10 * 1) / 2 - )
-  }
-}
-
-function handleMouseDown (divider) {
-  return function handler (event) {
-    event.preventDefault()
-    if (event.button !== 0) {
-      return;
-    }
-
-    const dragOffset = event.clientX - divider.element.getBoundingClientRect().left
-    // const formerElementStart = 
-    const previousSiblingStart = event.clientX - divider.element.previousSibling.getBoundingClientRect().left
-    const moveHandler = handleMouseMove(dragOffset, previousSiblingStart)
-    const test = () => {
-      console.log('Testing....')
-    }
+    const moveHandler = handleMouseMove(dragOffset, event, sizes, renderSize);
 
     document.addEventListener("mousemove", moveHandler);
     document.addEventListener("mouseup", handleMouseUp(moveHandler));
   }
-}
 
-export default function Tilt({ children: reactChilden }) {
-  const dividerReference = React.useRef();
+  const dividends = [];
 
-  // React.useEffect(() => {
-  //   const dividerNode = dividerReference.current;
+  for (let index = 0, len = Children.count(children); index < len; ++index) {
+    const child = children[index];
 
-  //   divider([...dividerNode.children]);
-  //   return () => {
-  //     console.log("Destroyed");
-  //   };
-  // }, []);
+    if (index % 2 !== 0)
+      dividends.push(
+        <div onMouseDown={handleDown} style={{ width: "20px" }}>
+          c
+        </div>
+      );
 
-  function oldMouseDownHandler(event) {
-    event.preventDefault();
-    if (event.button !== 0) {
-      return;
-    }
-
-    console.log(stuff)
-
-    // this.dragOffset = event.clientX - this.element.getBoundingClientRect().left;
-    // this.previousElementStartPosition = nodes[
-    //   this.previous
-    // ].element.getBoundingClientRect().left;
-
-    // nodes[this.previous].element.addEventListener("selectstart", noOperation);
-    // nodes[this.previous].element.addEventListener("dragstart", noOperation);
-    // nodes[this.next].element.addEventListener("selectstart", noOperation);
-    // nodes[this.next].element.addEventListener("dragstart", noOperation);
-
-    // nodes[this.previous].element.style.userSelect = "none";
-    // nodes[this.previous].element.style.webkitUserSelect = "none";
-    // nodes[this.previous].element.style.MozUserSelect = "none";
-    // nodes[this.previous].element.style.pointerEvents = "none";
-
-    // nodes[this.next].element.style.userSelect = "none";
-    // nodes[this.next].element.style.webkitUserSelect = "none";
-    // nodes[this.next].element.style.MozUserSelect = "none";
-    // nodes[this.next].element.style.pointerEvents = "none";
-
-    // document.addEventListener("mousemove", this.drag);
-    // document.addEventListener("mouseup", this.stop);
+    dividends.push(
+      cloneElement(child, {
+        style: { width: `calc(${sizes[index]}% - ${20 / 2}px)` },
+      })
+    );
   }
 
-  useEffect(() => {
-    let children = dividerReference.current.children;
-
-    // console.log(children)
-    children = [...children]
-    children.map((el, position) => {
-      const node = {
-        position,
-        el,
-        size: children.map(() => 100 / children.length),
-        minSize: children.map(() => 100),
-        mazSize: children.map(() => Number.POSITIVE_INFINITY),
-      };
-
-      // node.element.style.width = `calc(${node.size}% - ${
-      //   (10 * 2) / children.length
-      // }px)`;
-
-      if (position === 0) {
-        return node;
-      }
-
-      const divider = {
-        element: document.createElement("div"),
-        previous: position - 1,
-        next: position,
-      };
-
-      divider.element.style.width = `10px`;
-      divider.element.style.height = `10px`;
-      divider.element.className = "divider";
-      console.log(el)
-
-      el.parentNode.insertBefore(divider.element, node.element)
-      
-      // const work = closeOver(divider, mouseDownHandler)
-      const work = handleMouseDown(divider)
-  
-      
-      // divider.start = mouseDownHandler.bind(divider);
-      // divider.drag = mouseMoveHandler.bind(divider);
-      // divider.stop = mouseUpHandler.bind(divider);
-      divider.element.addEventListener("mousedown", work);
-      // divider.element.addEventListener("mousedown", work);
-
-      return node;
-
-    })
-  }, [])
-  
-
-  return <div ref={dividerReference}>{reactChilden}</div>;
+  return <div style={{ display: "flex" }}>{dividends}</div>;
 }
